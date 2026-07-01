60bdcac9b46e6873deca5d9a66971df3ed6d0cb6
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2026-07-01 19:47:48 +0200

    refactor(example): call requireDotnetModule in global ns

--- packages/example-module/src/index.ts
+++ packages/example-module/src/index.ts
@@ -1,13 +1,11 @@
 import { requireDotnetModule } from 'expo-modules-dotnet';
 
+const nativeModule = requireDotnetModule<ExampleModule>('ExampleModule');
+
 export type ExampleModule = {
   add(a: number, b: number): number;
 };
 
-export function requireExampleModule(): ExampleModule {
-  return requireDotnetModule<ExampleModule>('ExampleModule');
-}
-
 export function add(a: number, b: number): number {
-  return requireExampleModule().add(a, b);
+  return nativeModule.add(a, b);
 }
